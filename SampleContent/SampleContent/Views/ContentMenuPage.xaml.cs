14393dc593ea8b716abb42c9c5f3c3055be12922
Abraham Morillo <abrahammorillo777@gmail.com>
2020-02-12 14:14:38 -0400

    Pequeños cambios

--- SampleContent/SampleContent/Views/ContentMenuPage.xaml.cs
+++ SampleContent/SampleContent/Views/ContentMenuPage.xaml.cs
@@ -15,7 +15,7 @@ namespace SampleContent.Views
         public ContentMenu(string name)
         {
             InitializeComponent();
-            titulo.Title = $"Bienvenido {name}";          
+            TitlePage.Title = $"Bienvenido {name}";          
         }
     }
 }
